4d8b276de7f5e2f7d0a0af2c5c1a5d78694f0f76
YHANJARAMILLOD <yhjaramillod@unal.edu.co>
2026-07-05 22:22:02 -0500

    primera

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -3,10 +3,82 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
 # pylint: disable=import-outside-toplevel
-
-
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
 def pregunta_01():
-    """
+    plt.figure()
+    df=pd.read_csv('news.csv',index_col=0)
+    colors ={
+        'Television': 'dimgray',
+        'Newspaper': 'grey',
+        'Internet': 'tab: blue',
+        'Rodio' : "lightgrey",
+        }
+    zorder = {
+        'Television': 1,
+        'Newspaper': 1,
+        'Internet': 2,
+        'Radio': 1,
+    }
+    linewidths = {
+        'Television': 2,
+        'Newspaper' : 2,
+        'Internet': 4,
+        'Radio' : 2,
+        }
+    for col in df.columns:
+        plt.plot(df[col],color=colors[col], label=col, zorder=zorder[col],linewidths=linewidths[col],)
+    plt.title("How people get their news",fontsize=16)
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['left'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    plt.gca().axes.get_yaxis().set_visible(False)
+    for col in df.columns:
+        first_year = df.index[0]
+        plt.scatter(
+            x=first_year,
+            y=df [col] [first_year], color=colors[col], zorder=zorder[col],)
+        plt.text(
+            first_year - 0.2,
+            df[col][first_year],
+            col + " " + str(df [col] [first_year]) + "%",
+            ha='right',
+            va='center',
+            color=colors[col],
+)
+        last_year = df.index[-1]
+        plt.scatter(
+            x=last_year,
+            y=df [col] [last_year], color=colors[col], zorder=zorder[col],)
+        plt.text(
+            last_year + 0.2,
+            df[col][last_year],
+            col + " " + str(df [col] [last_year]) + "%",
+            ha='right',
+            va='center',
+            color=colors[col],
+)
+    plt.xticks(
+        ticks=df.index,
+        labels=df.index,
+        ha='center',)
+ruta_archivo = 'files/plots/news.png'
+ruta_carpeta = os.path.dirname(ruta_archivo)
+os.makedirs(ruta_carpeta, exist_ok=True)
+# 4. Guardar el gráfico en la ruta especificada
+plt.tight_layout()
+plt.savefig(ruta_archivo)
+
+
+    # plt.legend(loc="Upper right")
+    
+    
+
+
+
+
+"""
     Siga las instrucciones del video https://youtu.be/qVdwpxG_JpE para
     generar el archivo `files/plots/news.png`.
 
